8da3575cf09db4e035a946f0efde7b4ca8247808
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2020-12-03 11:00:58 -0500

    commit tree 23749476ffbf48bf9849f591200cc90a6b429723

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,47 @@
+require 'pry'
 class Transfer
-  # your code here
+  attr_accessor :status
+  attr_reader :sender, :receiver, :amount
+
+  @@all = []
+
+  def initialize(sender, receiver, amount)
+    @status = "pending"
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def valid? 
+    @sender.valid? && @receiver.valid?
+  end
+
+  def execute_transaction
+    if valid? && self.status == "pending" && sender.balance >= @amount
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else 
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      sender.balance += amount
+      receiver.balance -= amount
+      self.status = "reversed"
+    end
+  end
+
 end
